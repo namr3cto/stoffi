20a73a19e32c9b2326ae3030d8eefc07fb5b8be8
Christoffer Brodd-Reijer <christoffer@stoffiplayer.com>
2014-08-12 11:40:42 +0200

    website: make source class able to extract correct resource type from path

--- Website/app/models/source.rb
+++ Website/app/models/source.rb
@@ -7,28 +7,51 @@ class Source < ActiveRecord::Base
 		if path.start_with? 'stoffi:'
 			parts = path.split(':', 4)
 			{
-				resource: parts[1].to_sym,
+				resource: parse_resource(parts[1]),
 				source: parts[2].to_sym,
 				id: parts[3],
 			}
-		elsif path.start_with? 'http://' or path.start_with? 'https://'
-			{ source: :url, id: path }
 		else
-			{ source: :local, id: path }
+			ext = File.extname(path)
+			src = :local
+			src = :url if path.start_with? 'http://' or path.start_with? 'https://'
+			resource = 'song' if ext.in? SONG_EXT or (ext.empty? and src == :url)
+			resource = 'playlist' if ext.in? PLAYLIST_EXT
+			{ source: src, id: path, resource: resource }
 		end
 		rescue
-			raise path
 		end
 	end
 	
-	def self.get_by_path(path)
-		begin
-			path = parse_path(path) if path.is_a? String
-			find_or_create_by(name: path[:source], foreign_id: path[:id])
-			
-		rescue StandardError => e
-			raise e
-			logger.error "could not get source from path: #{e.message}"
-		end
+	def self.find_or_create_by_path(path)
+		find_or_create_by(path_to_find_hash(path))
 	end
+	
+	def self.find_by_path(path)
+		find_by(path_to_find_hash(path))
+	end
+	
+	private
+	
+	def self.parse_resource(resource)
+		resource = 'song' if resource == 'track'
+		resource.singularize.camelize
+	end
+	
+	def self.path_to_find_hash(path)
+		path = parse_path(path) if path.is_a? String
+		{
+			name: path[:source],
+			foreign_id: path[:id],
+			resource_type: parse_resource(path[:resource])
+		}
+	end
+	
+	SONG_EXT = ['.aac', '.ac3', '.aif', '.aiff', '.ape', '.apl', '.bwf', '.flac',
+		'.m1a', '.m2a', '.m4a', '.mov', '.mp+', '.mp1', '.mp2', '.mp3', '.mp3pro',
+		'.mp4', '.mpa', '.mpc', '.mpeg', '.mpg', '.mpp', '.mus', '.ofr', '.ofs', '.ogg',
+		'.spx', '.tta', '.wav', '.wv']
+	
+	PLAYLIST_EXT = ['.b4s', '.m3u', '.pls', '.ram', '.wpl', '.asx', '.wax',
+		'.wvx', '.m3u8', '.xspf', '.xml']
 end
