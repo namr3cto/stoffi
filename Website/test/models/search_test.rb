50118064361115778b98bc31b62b8ab65082e426
Christoffer Brodd-Reijer <christoffer@stoffiplayer.com>
2014-08-14 22:04:05 +0200

    website: add test for getting latest search

--- Website/test/models/search_test.rb
+++ Website/test/models/search_test.rb
@@ -30,4 +30,13 @@ class SearchTest < ActiveSupport::TestCase
 		assert_equal 5, s.length, "wrong number of suggestions returned"
 		assert_equal "Bobby Brown", s[0][:query], "first suggestion wasn't correct"
 	end
+	
+	test "should get latest search" do
+		s = searches(:bob_marley)
+		s.update_attribute(:updated_at, 2.days.ago)
+		date = Search.latest_search(s.query, s.categories, s.sources)
+		assert_equal s.updated_at, date, "Didn't get the correct date"
+		date = Search.latest_search('foo', '', '')
+		assert date > 1.minute.ago, "Didn't get Time.now for invalid query"
+	end
 end
