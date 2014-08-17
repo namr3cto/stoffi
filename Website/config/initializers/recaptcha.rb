f91814b9f9a9eef894acb51e51a1c117ce4753ce
Christoffer Brodd-Reijer <christoffer@stoffiplayer.com>
2014-08-17 10:01:14 +0200

    website: update recaptcha initializer to new format

--- Website/config/initializers/recaptcha.rb
+++ Website/config/initializers/recaptcha.rb
@@ -1,3 +1,4 @@
-# -*- encoding : utf-8 -*-
-ENV['RECAPTCHA_PUBLIC_KEY'] = Rails.application.secrets.recaptcha['public']
-ENV['RECAPTCHA_PRIVATE_KEY'] = Rails.application.secrets.recaptcha['private']
+Recaptcha.configure do |config|
+  config.public_key  = Rails.application.secrets.recaptcha['public']
+  config.private_key = Rails.application.secrets.recaptcha['private']
+end
