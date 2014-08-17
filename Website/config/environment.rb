72dd6c23aed597707e7b8e967e1ed9d906be6ba6
Christoffer Brodd-Reijer <christoffer@stoffiplayer.com>
2014-08-17 10:01:48 +0200

    website: remove forced development environment

--- Website/config/environment.rb
+++ Website/config/environment.rb
@@ -2,8 +2,6 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-Rails.env = 'development'
-
 # Make sure we use UTF-8
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
