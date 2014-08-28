447081c24cb94d335040c5fba7e8c30541767b4a
Christoffer Brodd-Reijer <christoffer@stoffiplayer.com>
2014-08-28 12:04:35 +0200

    website: allow genres to be ordered by listens and popularity

--- Website/app/models/genre.rb
+++ Website/app/models/genre.rb
@@ -13,11 +13,13 @@ class Genre < ActiveRecord::Base
 	include Base
 	include Imageable
 	include Sourceable
+	include Rankable
 	
 	has_and_belongs_to_many :songs, uniq: true
 	with_options through: :songs do |assoc|
 		assoc.has_many :artists
 		assoc.has_many :albums
+		assoc.has_many :listens
 	end
 	
 	validates :name, presence: true
--- Website/test/models/genre_test.rb
+++ Website/test/models/genre_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class GenreTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "should get top genres" do
+		g = Genre.top.limit(3)
+		assert_equal 3, g.length, "Didn't return three top genres"
+		assert g[0].listens.count >= g[1].listens.count, "Top genres not in order (first and second)"
+		assert g[1].listens.count >= g[2].listens.count, "Top genres not in order (second and third)"
+	end
 end
