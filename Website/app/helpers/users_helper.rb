27e68b527f550871b0a3ae09b10b60af1e5c42c5
Christoffer Brodd-Reijer <christoffer@stoffiplayer.com>
2015-01-08 21:59:47 +0100

    website: create user specific select option helpers
    Create two helpers, which calls options_for_select with special input,
    related to names and images for the current_user.
    
    name_options_for_select:
    Returns select options for choosing a name source.
    
    image_options_for_select
    Returns select options for choosing an avatar.

--- Website/app/helpers/users_helper.rb
+++ Website/app/helpers/users_helper.rb
@@ -7,4 +7,37 @@ module UsersHelper
 	def image_ads?
 		current_user == nil || (current_user && current_user.show_ads == "all")
 	end
+	
+	def name_options_for_select
+		options = []
+		
+		current_user.links.each do |l|
+			l.names.each do |label,name|
+				options << ["#{name} (#{l})", "#{l.provider}::#{label}"]
+			end
+		end
+		
+		options << [t('settings.custom'), '']
+		
+		options_for_select options, current_user.name_source
+	end
+	
+	def image_options_for_select
+		options = [[t('settings.default'), '', { data: { imagesrc: image_path('media/user.png') }}]]
+		
+		current_user.links.each do |l|
+			if l.picture? and l.picture
+				options << [l.display, l.provider, { data: { imagesrc: l.picture }}]
+			end
+		end
+		
+		[:mm, :identicon, :monsterid, :wavatar, :retro].each do |i|
+			name = i == :mm ? "Gravatar" : i.to_s
+			disp = name.titleize
+			disp = 'MonsterID' if name == 'monsterid'
+			options << [disp, name, { data: { imagesrc: current_user.gravatar(i) }}]
+		end
+		
+		options_for_select options, current_user.image
+	end
 end
