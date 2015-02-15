736ba9c4987b8ab306c921c3e3368a2bff63ef7b
Christoffer Brodd-Reijer <christoffer@stoffiplayer.com>
2015-02-15 22:48:02 +0100

    website: clean up songs controller
    * Remove unused formats
    * Refactor setting of @song
    * Create different top lists in #index
    * Move to strong parameters

--- Website/app/controllers/songs_controller.rb
+++ Website/app/controllers/songs_controller.rb
@@ -9,68 +9,41 @@
 # License::		GNU General Public License (stoffiplayer.com/license)
 
 class SongsController < ApplicationController
+	before_action :set_song, only: [:show, :edit, :update, :destroy]
 
 	oauthenticate interactive: true, except: [ :index, :show ]
 	before_filter :ensure_admin, except: [ :index, :show, :create, :destroy ]
-	respond_to :html, :mobile, :embedded, :xml, :json
+	respond_to :html, :xml, :json
 	
 	# GET /songs
 	def index
-		l = params[:limit] || 25
-		o = params[:offset] || 0
-		if logged_in?
-			@songs = current_user.songs.limit(l).offset(o)
-		else
-			@songs = Song.limit(l).offset(o)
+		@max_items = 12
+		
+		@recent = Listen.order(created_at: :desc).limit(@max_items).map(&:song)
+		@weekly = Song.top(from: 7.days.ago).limit @max_items
+		@alltime = Song.top.limit @max_items
+		
+		if user_signed_in?
+			@user_recent = current_user.listens.order(created_at: :desc).limit(@max_items).map(&:song)
+			@user_weekly = Song.top(for: current_user, from: 7.days.ago).limit @max_items
+			@user_alltime = Song.top(for: current_user).limit @max_items
 		end
 
-		respond_with(@songs)
+		respond_with(@weekly)
 	end
 
 	# GET /songs/1
 	def show
-		not_found('song') and return unless Song.exists? params[:id]
-		@song = Song.find(params[:id])
-		@title = CGI.unescapeHTML(@song.pretty_name)
-		if @song.artist
-			@description = t "songs.description", song: @song.title, artist: @song.artist.name
-		else
-			@description = t "songs.description_without_artist", song: @song.title
-		end
-		@head_prefix = "og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# stoffiplayer: http://ogp.me/ns/fb/stoffiplayer#"
-		@meta_tags =
-		[
-			{ property: "og:title", content: d(@song.title) },
-			{ property: "og:type", content: "music.song" },
-			{ property: "og:image", content: @song.picture },
-			{ property: "og:url", content: @song.url },
-			{ property: "og:description", content: d(@description) },
-			{ property: "og:site_name", content: "Stoffi" },
-			{ property: "fb:app_id", content: "243125052401100" },
-			{ property: "music:duration", content: @song.length.to_i },
-			{ property: "og:audio", content: @song.play },
-			{ property: "og:audio:type", content: "audio/vnd.facebook.bridge" }
-		] |
-			@song.artists.map { |artist| { property: "music:musician", content: artist.url } } |
-			@song.albums.map { |album| { property: "music:album", content: album.url } }
-
 		respond_with(@song, include: :artists)
 	end
 
 	# GET /songs/new
 	def new
 		redirect_to songs_path and return
-		
-		@title = "Add song"
-		@description = "Add a new song to your collection"
-		@song = current_user.songs.new
-
-		respond_with(@song)
 	end
 
 	# GET /songs/1/edit
 	def edit
-		@song = current_user.songs.find(params[:id])
 		redirect_to song_path(@song)
 	end
 
@@ -89,14 +62,12 @@ class SongsController < ApplicationController
 
 	# PUT /songs/1
 	def update
-		@song = Song.find(params[:id])
-		@song.update_attributes(params[:song])
+		@song.update_attributes(song_params)
 		respond_with(@song)
 	end
 
 	# DELETE /songs/1
 	def destroy
-		@song = Song.find(params[:id])
 		current_user.songs.delete(@song)
 		
 		unless @song.users.count > 0 || @song.youtube? || @song.soundcloud?
@@ -104,5 +75,18 @@ class SongsController < ApplicationController
 		end
 		
 		respond_with(@song)
+	end
+	
+	private
+
+	# Use callbacks to share common setup or constraints between actions.
+	def set_song
+		not_found('song') and return unless Song.exists? params[:id]
+		@song = Song.find(params[:id])
+	end
+
+	# Never trust parameters from the scary internet, only allow the white list through.
+	def song_params
+		params.require(:song).permit(:title, :genres, :artists)
 	end
 end
