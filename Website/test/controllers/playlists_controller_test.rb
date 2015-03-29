8eee0ec6183f6684e1ee5bf8d74684dcc90ebb3e
Christoffer Brodd-Reijer <christoffer@stoffiplayer.com>
2015-03-29 20:57:39 +0200

    make playlist controller tests pass

--- Website/test/controllers/playlists_controller_test.rb
+++ Website/test/controllers/playlists_controller_test.rb
@@ -2,6 +2,7 @@ require 'test_helper'
 
 class PlaylistsControllerTest < ActionController::TestCase
 	include Devise::TestHelpers
+	
 	setup do
 		@playlist = users(:alice).playlists.first
 		@playlist2 = users(:bob).playlists.first
@@ -13,33 +14,39 @@ class PlaylistsControllerTest < ActionController::TestCase
 		sign_in @user
 		get :index
 		assert_response :success
-		assert_not_nil assigns(:personal)
-		assert_not_nil assigns(:global)
-		assert_equal @user.playlists.count, assigns(:personal).count
+		assert_not_nil assigns(:user_owns)
+		assert_not_nil assigns(:all_time)
+		assert_equal @user.playlists.count, assigns(:user_owns).count
 	end
 
 	test "should get index logged out" do
 		get :index
 		assert_response :success
-		assert_nil assigns(:personal)
-		assert_not_nil assigns(:global)
+		assert_nil assigns(:user_owns)
+		assert_not_nil assigns(:all_time)
 	end
-
-	test "should show" do
-		get :show, :id => @playlist.to_param
+	
+	test "should get show logged in" do
+		sign_in @user
+		get :show, id: @playlist.to_param
 		assert_response :success
 	end
-
+	
+	test "should get show logged out" do
+		get :show, id: @playlist.to_param
+		assert_response :success
+	end
+	
 	test "should not show" do
-		get :show, :id => "foobar"
+		get :show, id: "foobar"
 		assert_response :not_found
 	end
 
 	test "should not get new logged out" do
 		get :new
-		assert_redirected_to login_url, "Not redirected to login page"
+		assert_redirected_to new_user_session_path, "Not redirected to login page"
 	end
-
+	
 	test "should get new" do
 		sign_in @user
 		get :new
@@ -64,7 +71,7 @@ class PlaylistsControllerTest < ActionController::TestCase
 
 	test "should not create playlist logged out" do
 		post :create, :playlist => @playlist.attributes
-		assert_redirected_to login_url, "Not redirected to login page"
+		assert_redirected_to new_user_session_path, "Not redirected to login page"
 	end
 
 	test "should not create two playlists with same name" do
@@ -85,37 +92,37 @@ class PlaylistsControllerTest < ActionController::TestCase
 
 	test "should get edit" do
 		sign_in @user
-		get :edit, :id => @playlist.to_param
+		get :edit, id: @playlist.to_param
 		assert_response :success
 	end
 
 	test "should not get edit logged out" do
-		get :edit, :id => @playlist.to_param
-		assert_redirected_to login_url, "Not redirected to login page"
+		get :edit, id: @playlist.to_param
+		assert_redirected_to new_user_session_path, "Not redirected to login page"
 	end
 
 	test "should not get edit for someone else's playlist" do
 		sign_in @user
-		get :edit, :id => @playlist2.to_param
+		get :edit, id: @playlist2.to_param
 		assert_response :not_found
 	end
 
 	test "should update playlist" do
 		Link.any_instance.stubs(:update_playlist).returns(nil)
 		sign_in @user
-		put :update, :id => @playlist.to_param, :playlist => @playlist.attributes
+		put :update, id: @playlist.to_param, :playlist => @playlist.attributes
 		assert_response :found
 		assert_redirected_to playlist_path(assigns(:playlist))
 	end
 
 	test "should not update playlist logged out" do
-		put :update, :id => @playlist.to_param, :playlist => @playlist.attributes
-		assert_redirected_to login_url, "Not redirected to login page"
+		put :update, id: @playlist.to_param, :playlist => @playlist.attributes
+		assert_redirected_to new_user_session_path, "Not redirected to login page"
 	end
 
 	test "should not update someone else's playlist" do
 		sign_in @user
-		put :update, :id => @playlist2.to_param, :playlist => @playlist2.attributes
+		put :update, id: @playlist2.to_param, :playlist => @playlist2.attributes
 		assert_response :not_found
 	end
 
@@ -123,32 +130,32 @@ class PlaylistsControllerTest < ActionController::TestCase
 		Link.any_instance.stubs(:delete_playlist).returns(nil)
 		sign_in @user
 		assert_difference('Playlist.count', -1) do
-			delete :destroy, :id => @playlist.to_param
+			delete :destroy, id: @playlist.to_param
 		end
 		assert_redirected_to playlists_path
 	end
 
 	test "should not destroy playlist logged out" do
-		delete :destroy, :id => @playlist.to_param
-		assert_redirected_to login_url, "Not redirected to login page"
+		delete :destroy, id: @playlist.to_param
+		assert_redirected_to new_user_session_path, "Not redirected to login page"
 	end
-	
+		
 	test "should follow playlist" do
 		sign_in @user
-		put :follow, :id => @playlist2.to_param
+		put :follow, id: @playlist2.to_param
 		assert_response :found
-		assert @playlist2.subscribers.exists?(@user.id), "Not subscribed to playlist"
+		assert @user.follows?(@playlist2), "Not following playlist"
 	end
 
 	test "should unfollow playlist" do
 		sign_in @user
-		put :follow, :id => @playlist2.to_param
+		put :follow, id: @playlist2.to_param
 		assert_redirected_to playlist_path(@playlist2), "Could not follow playlist"
-		assert @playlist2.subscribers.exists?(@user.id), "Not subscribed to playlist"
-		delete :destroy, :id => @playlist2.to_param
+		assert @user.follows?(@playlist2), "Not following playlist"
+		delete :destroy, id: @playlist2.to_param
 		assert_redirected_to playlist_path(@playlist2), "Could not unfollow playlist"
-		get :show, :id => @playlist2.to_param
+		get :show, id: @playlist2.to_param
 		assert_response :success, "Could not get playlist"
-		assert !(@playlist2.subscribers.exists?(@user.id)), "Still subscribing to playlist"
+		assert_not @user.follows?(@playlist2), "Still following playlist"
 	end
 end
