68be9f721c8aa6689aef846dfddaac073d62c6ee
Christoffer Brodd-Reijer <christoffer@stoffiplayer.com>
2015-04-01 14:45:57 +0200

    fix event controller

--- Website/app/controllers/events_controller.rb
+++ Website/app/controllers/events_controller.rb
@@ -1,74 +1,83 @@
+# The business logic for events.
+#
+# This code is part of the Stoffi Music Player Project.
+# Visit our website at: stoffiplayer.com
+#
+# Author::      Christoffer Brodd-Reijer (christoffer@stoffiplayer.com)
+# Copyright::   Copyright (c) 2015 Simplare
+# License::     GNU General Public License (stoffiplayer.com/license)
+
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
+	
+	before_action :set_event, only: [:show, :edit, :update, :destroy]
+	before_action :ensure_admin, only: [ :update, :destroy ]
+	oauthenticate interactive: true, except: [ :index, :show ]
 
-  # GET /events
-  # GET /events.json
-  def index
-    @events = Event.all
-  end
+	# GET /events
+	def index
+		l, o = pagination_params
+		@events = Event.order(created_at: :desc).limit(l).offset(o)
+		
+		if user_signed_in?
+			artist_ids = []
+			artists = Artist.top(for: current_user, from: 7.days.ago).limit(l)
+			artists.each { |a| artist_ids << a.id }
+			where_clause = artist_ids.map { |id| "artists_genres.artist_id = #{id}" }.join(' OR ')
+			@user_events = Event.joins(:artists).where(where_clause).uniq.limit(l).offset(o)
+		end
+	end
 
-  # GET /events/1
-  # GET /events/1.json
-  def show
-  end
+	# GET /events/1
+	def show
+	end
 
-  # GET /events/new
-  def new
-    @event = Event.new
-  end
+	# POST /events
+	def create
+		@event = Event.new(event_params)
 
-  # GET /events/1/edit
-  def edit
-  end
+		respond_to do |format|
+			if @event.save
+				format.html { redirect_to @event, notice: 'Event was successfully created.' }
+				format.json { render :show, status: :created, location: @event }
+			else
+				format.html { render :new }
+				format.json { render json: @event.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  # POST /events
-  # POST /events.json
-  def create
-    @event = Event.new(event_params)
+	# PATCH/PUT /events/1
+	def update
+		respond_to do |format|
+			if @event.update(event_params)
+				format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+				format.json { render :show, status: :ok, location: @event }
+			else
+				format.html { render :edit }
+				format.json { render json: @event.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-    respond_to do |format|
-      if @event.save
-        format.html { redirect_to @event, notice: 'Event was successfully created.' }
-        format.json { render :show, status: :created, location: @event }
-      else
-        format.html { render :new }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	# DELETE /events/1
+	def destroy
+		@event.destroy
+		respond_to do |format|
+			format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
+			format.json { head :no_content }
+		end
+	end
 
-  # PATCH/PUT /events/1
-  # PATCH/PUT /events/1.json
-  def update
-    respond_to do |format|
-      if @event.update(event_params)
-        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
-        format.json { render :show, status: :ok, location: @event }
-      else
-        format.html { render :edit }
-        format.json { render json: @event.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	private
+	
+	# Use callbacks to share common setup or constraints between actions.
+	def set_event
+		not_found('event') and return unless Event.exists? params[:id]
+		@event = Event.find(params[:id])
+	end
 
-  # DELETE /events/1
-  # DELETE /events/1.json
-  def destroy
-    @event.destroy
-    respond_to do |format|
-      format.html { redirect_to events_url, notice: 'Event was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = Event.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def event_params
-      params.require(:event).permit(:name, :venue, :latitude, :longitude, :start, :stop, :content, :category)
-    end
+	# Never trust parameters from the scary internet, only allow the white list through.
+	def event_params
+		params.require(:event).permit(:name, :venue, :latitude, :longitude, :start, :stop, :content, :category)
+	end
 end
--- Website/test/controllers/events_controller_test.rb
+++ Website/test/controllers/events_controller_test.rb
@@ -1,49 +1,130 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionController::TestCase
-  setup do
-    @event = events(:festival)
-  end
+	include Devise::TestHelpers
+	
+	setup do
+		@admin = users(:alice)
+		@user = users(:bob)
+		@event = events(:festival)
+		@event_params = {
+			content: 'Test content',
+			start: 3.days.from_now,
+			latitude: -1,
+			longitude: -1,
+			name: 'New event',
+			category: 'Test category',
+			stop: 4.days.from_now,
+			venue: 'Test venue'
+		}
+	end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:events)
-  end
+	test "should get index logged in" do
+		sign_in @user
+		get :index
+		assert_response :success
+		assert_not_nil assigns(:events)
+		assert_not_nil assigns(:user_events)
+	end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+	test "should get index logged out" do
+		get :index
+		assert_response :success
+		assert_nil assigns(:user_events)
+		assert_not_nil assigns(:events)
+	end
+	
+	test "should get show logged in" do
+		sign_in @user
+		get :show, id: @event
+		assert_response :success
+	end
+	
+	test "should get show logged out" do
+		get :show, id: @event
+		assert_response :success
+	end
+	
+	test "should not show missing" do
+		get :show, id: "foobar"
+		assert_response :not_found
+	end
 
-  test "should create event" do
-    assert_difference('Event.count') do
-      post :create, event: { content: @event.content, start: @event.start, latitude: @event.latitude, longitude: @event.longitude, name: 'New event', category: @event.category, stop: @event.stop, venue: @event.venue }
-    end
+	test "should not get new" do
+		assert_raises AbstractController::ActionNotFound do
+			get :new
+		end
+	end
+	
+	test "should not create logged out" do
+		assert_no_difference('Event.count') do
+			post :create, event: @event_params
+		end
+		assert_redirected_to new_user_session_path
+	end
 
-    assert_redirected_to event_path(assigns(:event))
-  end
+	test "should create as user" do
+		sign_in @user
+		assert_difference('Event.count') do
+			post :create, event: @event_params
+		end
+		assert_redirected_to event_path(assigns(:event))
+	end
 
-  test "should show event" do
-    get :show, id: @event
-    assert_response :success
-  end
+	test "should create as admin" do
+		sign_in @admin
+		assert_difference('Event.count') do
+			post :create, event: @event_params
+		end
+		assert_redirected_to event_path(assigns(:event))
+	end
 
-  test "should get edit" do
-    get :edit, id: @event
-    assert_response :success
-  end
+	test "should not get edit" do
+		assert_raises AbstractController::ActionNotFound do
+			get :edit, id: @event
+		end
+	end
 
-  test "should update event" do
-    patch :update, id: @event, event: { content: @event.content, start: @event.start, latitude: @event.latitude, longitude: @event.longitude, name: @event.name, category: @event.category, stop: @event.stop, venue: @event.venue }
-    assert_redirected_to event_path(assigns(:event))
-  end
+	test "should not update logged out" do
+		patch :update, id: @event, event: { name: 'New name' }
+		assert_redirected_to new_user_session_path
+		assert_not_equal 'New name', Event.find(@event.id).name, "Changed name"
+	end
 
-  test "should destroy event" do
-    assert_difference('Event.count', -1) do
-      delete :destroy, id: @event
-    end
+	test "should not update as user" do
+		sign_in @user
+		patch :update, id: @event, event: { name: 'New name' }
+		assert_redirected_to dashboard_path
+		assert_not_equal 'New name', Event.find(@event.id).name, "Changed name"
+	end
 
-    assert_redirected_to events_path
-  end
+	test "should update as admin" do
+		sign_in @admin
+		patch :update, id: @event, event: { name: 'New name' }
+		assert_redirected_to event_path(assigns(:event))
+		assert_equal 'New name', Event.find(@event.id).name, "Didn't change name"
+	end
+
+	test "should not destroy logged out" do
+		assert_no_difference('Event.count') do
+			delete :destroy, id: @event
+		end
+		assert_redirected_to new_user_session_path
+	end
+
+	test "should not destroy as user" do
+		sign_in @user
+		assert_no_difference('Event.count') do
+			delete :destroy, id: @event
+		end
+		assert_redirected_to dashboard_path
+	end
+
+	test "should destroy as admin" do
+		sign_in @admin
+		assert_difference('Event.count', -1) do
+			delete :destroy, id: @event
+		end
+		assert_redirected_to events_path
+	end
 end
