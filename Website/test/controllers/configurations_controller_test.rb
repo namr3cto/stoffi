6dc72cbdb7b10c49a4800ee44c1c9ef34ca23f90
Christoffer Brodd-Reijer <christoffer@stoffiplayer.com>
2015-04-28 19:25:26 +0200

    create tests for configurations controller

--- Website/test/controllers/configurations_controller_test.rb
+++ Website/test/controllers/configurations_controller_test.rb
@@ -19,7 +19,7 @@ class ConfigurationsControllerTest < ActionController::TestCase
 	test "should get show logged in" do
 		sign_in @user
 		get :show, id: @config
-		assert_response :success
+		assert_redirected_to remote_path
 	end
 	
 	test "should not get show logged out" do
@@ -51,70 +51,38 @@ class ConfigurationsControllerTest < ActionController::TestCase
 		assert_difference('Configuration.count') do
 			post :create, configuration: { name: 'Foo' }
 		end
-		assert_redirected_to configuration_path(assigns(:configuration))
+		assert_redirected_to configuration_path(assigns(:config))
 	end
 
 	test "should not get edit" do
 		assert_raises AbstractController::ActionNotFound do
-			get :edit, id: @configuration
+			get :edit, id: @config
 		end
 	end
 
 	test "should not update when logged out" do
-		patch :update, id: @configuration, configuration: { name: 'New name' }
+		patch :update, id: @config, configuration: { name: 'New name' }
 		assert_redirected_to new_user_session_path
-		assert_not_equal 'New name', Configuration.find(@configuration.id).name, "Changed name"
+		assert_not_equal 'New name', Configuration.find(@config.id).name, "Changed name"
 	end
 
 	test "should update as owner" do
 		sign_in @user
-		patch :update, id: @configuration, configuration: { name: 'New name' }
-		assert_redirected_to configuration_path(assigns(:configuration))
-		assert_equal 'New name', Configuration.find(@configuration.id).name, "Didn't change name"
+		patch :update, id: @config, configuration: { name: 'New name' }
+		assert_redirected_to configuration_path(assigns(:config))
+		assert_equal 'New name', Configuration.find(@config.id).name, "Didn't change name"
 	end
 
 	test "should not update someone elses" do
 		sign_in @user
 		patch :update, id: @admin.configurations.first, configuration: { name: 'New name' }
-		assert_redirected_to dashboard_path
+		assert_response :not_found
 		assert_not_equal 'New name', Configuration.find(@admin.configurations.first.id).name, "Changed name"
 	end
 
-	test "should update as admin" do
-		sign_in @admin
-		patch :update, id: @configuration, configuration: { name: 'New name' }
-		assert_redirected_to configuration_path(assigns(:configuration))
-		assert_equal 'New name', Configuration.find(@configuration.id).name, "Didn't change name"
-	end
-
-	test "should not destroy when logged out" do
-		assert_no_difference('Configuration.count') do
-			delete :destroy, id: @configuration
+	test "should not destroy" do
+		assert_raises AbstractController::ActionNotFound do
+			get :destroy, id: @config
 		end
-		assert_redirected_to new_user_session_path
-	end
-
-	test "should not destroy someone elses" do
-		sign_in @user
-		assert_no_difference('Configuration.count') do
-			delete :destroy, id: @admin.configurations.first
-		end
-		assert_redirected_to dashboard_path
-	end
-
-	test "should destroy as owner" do
-		sign_in @user
-		assert_difference('Configuration.count', -1) do
-			delete :destroy, id: @configuration
-		end
-		assert_redirected_to configurations_path
-	end
-
-	test "should destroy as admin" do
-		sign_in @admin
-		assert_difference('Configuration.count', -1) do
-			delete :destroy, id: @configuration
-		end
-		assert_redirected_to configurations_path
 	end
 end
