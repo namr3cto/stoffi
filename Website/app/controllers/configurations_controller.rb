7e4a2404237ace7417f529c3c73f6e8b0c7a423c
Christoffer Brodd-Reijer <christoffer@stoffiplayer.com>
2015-04-28 19:25:55 +0200

    fix configurations controller

--- Website/app/controllers/configurations_controller.rb
+++ Website/app/controllers/configurations_controller.rb
@@ -5,112 +5,97 @@
 # Visit our website at: stoffiplayer.com
 #
 # Author::		Christoffer Brodd-Reijer (christoffer@stoffiplayer.com)
-# Copyright::	Copyright (c) 2013 Simplare
+# Copyright::	Copyright (c) 2015 Simplare
 # License::		GNU General Public License (stoffiplayer.com/license)
 
 class ConfigurationsController < ApplicationController
 
-	#before_filter :authenticate_user!
 	oauthenticate
-	respond_to :html, :mobile, :embedded, :xml, :json
-	
-	# GET /configurations
-	def index
-		l, o = pagination_params
-		@title = t("remote.title")
-		@description = t("remote.description")
-		@configurations = current_user.configurations.limit(l).offset(o)
-		respond_with(@configurations)
-	end
+	before_action :set_config, only: [:show, :update, :destroy, :next, :prev, :play,
+		:pause, :play_pause]
 
 	# GET /configurations/1
 	def show
-		@configuration = current_user.configurations.find(params[:id])
-		@devices = current_user.devices.order(:name)
-		@title = t("remote.title")
-		@description = t("remote.description")
-		respond_with(@configuration)
-	end
-
-	# GET /configurations/new
-	def new
-		@configuration = current_user.configurations.new
-		respond_with(@configuration)
-	end
-
-	# GET /configurations/1/edit
-	def edit
-		render :text => "not yet" and return
-		@configuration = current_user.configurations.find(params[:id])
-		@title = "Edit synchronization profile '#{h(@configuration.name)}'"
-		@description = "Modify the synchronization profile named #{h(@configuration.name)}"
+		respond_to do |format|
+			format.html { redirect_to remote_path }
+			format.json { render }
+		end
 	end
 
 	# POST /configurations
 	def create
-		@configuration = current_user.configurations.new(params[:configuration])
-		
-		success = @configuration.save
-		SyncController.send('create', @configuration, request) if success
-		respond_with(@configuration)
-	end
-
-	# PUT /configurations/1
-	def update
-		@configuration = current_user.configurations.find(params[:id])
-		
-		song = nil
-		if params[:configuration] && params[:configuration][:current_track]
-			song = Song.get(current_user, params[:configuration][:current_track])
-			params[:configuration][:current_track_id] = song.id if song.is_a?(Song)
-			params[:configuration].delete(:current_track)
+		@config = current_user.configurations.new(config_params)
+		success = @config.save
+		SyncController.send('create', @config, request) if success
+		respond_to do |format|
+			if success
+				format.html { redirect_to @config, notice: 'Configuration was successfully created.' }
+				format.json { render :show, status: :created, location: @config }
+			else
+				format.html { render dashboard_path }
+				format.json { render json: @config.errors, status: :unprocessable_entity }
+			end
 		end
-		success = @configuration.update_attributes(params[:configuration])
-		params[:configuration][:now_playing] = song.full_name if song.is_a?(Song)
-		SyncController.send('update', @configuration, request, params[:configuration]) if success
-		respond_with(@configuration)
 	end
 
-	# DELETE /configurations/1
-	def destroy
-		@configuration = current_user.configurations.find(params[:id])
-		SyncController.send('delete', @configuration, request) if success
-		@configuration.destroy
-		respond_with(@configuration)
+	# PATCH /configurations/1
+	def update
+		song = nil
+		if config_params[:current_track]
+			song = Song.get(current_user, config_params[:current_track])
+			config_params[:current_track_id] = song.id if song.is_a?(Song)
+			config_params.delete(:current_track)
+		end
+		success = @config.update_attributes(config_params)
+		config_params[:now_playing] = song.full_name if song.is_a?(Song)
+		SyncController.send('update', @config, request, config_params) if success
+		respond_to do |format|
+			if success
+				format.html { redirect_to @config, notice: 'Configuration was successfully updated.' }
+				format.json { render :show, status: :ok, location: @config }
+			else
+				format.html { render :show }
+				format.json { render json: @config.errors, status: :unprocessable_entity }
+			end
+		end
 	end
 	
 	# PUT /configurations/1/next
 	def next
-		@configuration = current_user.configurations.find(params[:id])
-		SyncController.send('execute', @configuration, request, 'next')
-		respond_with(@configuration)
+		SyncController.send('execute', @config, request, 'next')
 	end
 	
 	# PUT /configurations/1/prev
 	def prev
-		@configuration = current_user.configurations.find(params[:id])
-		SyncController.send('execute', @configuration, request, 'prev')
-		respond_with(@configuration)
+		SyncController.send('execute', @config, request, 'prev')
 	end
 	
 	# PUT /configurations/1/play-pause
 	def play_pause
-		@configuration = current_user.configurations.find(params[:id])
-		SyncController.send('execute', @configuration, request, 'play-pause')
-		respond_with(@configuration)
+		SyncController.send('execute', @config, request, 'play-pause')
 	end
 	
 	# PUT /configurations/1/play
 	def play
-		@configuration = current_user.configurations.find(params[:id])
-		SyncController.send('execute', @configuration, request, 'play')
-		respond_with(@configuration)
+		SyncController.send('execute', @config, request, 'play')
 	end
 	
 	# PUT /configurations/1/pause
 	def pause
-		@configuration = current_user.configurations.find(params[:id])
-		SyncController.send('execute', @configuration, request, 'pause')
-		respond_with(@configuration)
+		SyncController.send('execute', @config, request, 'pause')
+	end
+	
+	private
+	
+	# Use callbacks to share common setup or constraints between actions.
+	def set_config
+		not_found('config') and return unless current_user.configurations.exists? params[:id]
+		@config = current_user.configurations.find(params[:id])
+	end
+
+	# Never trust parameters from the scary internet, only allow the white list through.
+	def config_params
+		params.require(:configuration).permit(:name, :media_state, :shuffle, :repeat,
+			:current_track)
 	end
 end
